28aeb2f409008ff9b6d99a831631ab15c8e87011
Stephan Kerscher <stephan.kerscher@stud.uni-regensburg.de>
2017-03-21 18:56:51 +0100

    2

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello!!!!!!!!!!!!!!')
+    response.sendRedirect("index.html")
 })
 
 app.listen(app.get('port'), function() {
